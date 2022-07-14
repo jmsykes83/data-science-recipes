87aabc871d3b9e2592ed4433fa4d383c035243f4
jmsykes83 <108375876+jmsykes83@users.noreply.github.com>
2022-07-14 17:50:47 -0400

    Create models.py
    Created a model file

---
+++ models.py
@@ -0,0 +1,9 @@
+"""
+Useful models will be added to this file.
+All models need to be placed in functions or classes.
+Nothing should be global.
+
+Creator: Jason Sykes
+Email: jmsykes83@yahoo.com
+
+"""
