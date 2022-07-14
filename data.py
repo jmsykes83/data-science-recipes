5536f12b7e15aaa11a0e2d071cb025b4e7df56e7
jmsykes83 <108375876+jmsykes83@users.noreply.github.com>
2022-07-14 17:54:06 -0400

    Create data.py
    data

---
+++ data.py
@@ -0,0 +1,8 @@
+"""
+Useful data manipulation and extraction tools will be added to this file.
+All tools need to be placed in functions or classes.
+Nothing should be global.
+
+Creator: Jason Sykes
+Email: jmsykes83@yahoo.com
+"""
