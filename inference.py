6efc1108eaa77b8ceea04bf074305d11b7112ce9
jmsykes83 <108375876+jmsykes83@users.noreply.github.com>
2022-07-14 17:57:24 -0400

    Create inference.py
    inference

---
+++ inference.py
@@ -0,0 +1,8 @@
+"""
+Useful inference recipes will be added to this file.
+All recipes need to be placed in functions or classes.
+Nothing should be global.
+
+Creator: Jason Sykes
+Email: jmsykes83@yahoo.com
+"""
